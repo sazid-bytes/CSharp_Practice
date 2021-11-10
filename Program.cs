1c5e41990059bca5972767fd9ab7811191893ee8
Sazid <sazid.sunny@gmail.com>
2021-11-10 11:41:44 +0530

    Intial Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GitDemo1
+{
+    class Customer
+    {
+        String _fname;
+        string _lname;
+
+        public Customer(string firstname, string lastname)
+        {
+            _fname = firstname;
+            _lname = lastname;
+        }
+
+        public string PrintFullName()
+        {
+            return _fname + " " + _lname;
+        }
+        ~Customer()
+        {
+
+        }
+    }   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer("Sazid", "Shaik");
+            Console.WriteLine(c1.PrintFullName());
+        }
+        
+    }
+}
